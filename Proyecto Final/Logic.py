811e70c9e1d3981742f8240336c34f2312822b87
Felipe <dfzunigah@unal.edu.co>
2023-03-04 21:29:33 -0500

    Deleted Implication class because wasn't used

--- Proyecto Final/Logic.py
+++ Proyecto Final/Logic.py
@@ -170,40 +170,6 @@ class Or(Sentence):
     def symbols(self):
         return set.union(*[disjunct.symbols() for disjunct in self.disjuncts])
 
-
-class Implication(Sentence):
-    """Operador IMPLICACIÓN"""
-
-    def __init__(self, antecedent, consequent):
-        Sentence.validate(antecedent)
-        Sentence.validate(consequent)
-        self.antecedent = antecedent
-        self.consequent = consequent
-
-    def __eq__(self, other):
-        return (isinstance(other, Implication)
-                and self.antecedent == other.antecedent
-                and self.consequent == other.consequent)
-
-    def __hash__(self):
-        return hash(("implies", hash(self.antecedent), hash(self.consequent)))
-
-    def __repr__(self):
-        return f"Implication({self.antecedent}, {self.consequent})"
-
-    def evaluate(self, model):
-        return ((not self.antecedent.evaluate(model))
-                or self.consequent.evaluate(model))
-
-    def formula(self):
-        antecedent = Sentence.parenthesize(self.antecedent.formula())
-        consequent = Sentence.parenthesize(self.consequent.formula())
-        return f"{antecedent} => {consequent}"
-
-    def symbols(self):
-        return set.union(self.antecedent.symbols(), self.consequent.symbols())
-
-
 class Biconditional(Sentence):
     """Operador BICONDICIONAL"""
 
